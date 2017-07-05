922954b3f7c6fb1d84599ab64dcb3a89a1b672b4
Ashwin <ashwinchetty@gmail.com>
2017-07-04 20:08:29 -0500

    rewrite front page

--- html/index.php
+++ html/index.php
@@ -7,7 +7,7 @@
 ?>
 <html ng-app="healthapp">
 <head>
-<title>Eku Ojumo</title>
+<title>Neolafia</title>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
@@ -50,22 +50,20 @@ $(document).ready(function(){
   $('#ikeyword_search')
     .attr('placeholder',input_plc);
     
-  var input= $('#ikeyword_search')[0];
-  var searchbox = new Awesomplete(input, {
-    list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
-  });
 });
 
 
 </script>
+<link href="https://fonts.googleapis.com/css?family=Poiret+One|Quicksand|Zilla+Slab|Cabin" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="forms.css"> 
+<link rel="stylesheet" type="text/css" href="styles/styles.css"> 
 
 </head>
 
-<body ng-controller="HealthController" style="background: #0a6a8e">
+<body ng-controller="HealthController" style="background: #97d23b">
   <span ng-init='init_view()' />
-  <div style="justify-content: center; margin:auto; width:80%; padding:10px; display:block; margin-left:auto; margin-right:auto; border:3px solid green; margin: 0 auto; background:#2a7aae; min-height:100% ">
 <?php if($_SESSION['valid']): ?>
+  <div style="justify-content: center; margin:auto; width:80%; padding:10px; display:block; margin-left:auto; margin-right:auto; border:3px solid green; margin: 0 auto; background:#2a7aae; min-height:100% ">
 
   <div class="loggedinheader" style="">
     <div style="vertical-align:middle">
@@ -85,57 +83,54 @@ $(document).ready(function(){
   <hr>
 
   <table style="width:100%">
-  <tr>
-  <td class="sidepanel" style="">
-    <div id="e_search" ng-click="lcurrent_view='views/view_search.php'"             class="panelentry clickme">Search</div>
-    <div id="e_profile" ng-click="lcurrent_view='views/view_profile.php'"           class="panelentry clickme">My Profile</div>
-    <div id="e_patientappts" ng-click="lcurrent_view='views/view_patientappts.php'" class="panelentry clickme">My Appointments (Pt)</div>
-    <div id="e_doctorappts" ng-click="lcurrent_view='views/view_doctorappts.php'"   class="panelentry clickme">My Appointments (Dr)</div>
-  </td>
-  <td id="c_navpages" class="form-style-8" style="overflow:hidden">
-    <ng-include src="lcurrent_view"> </ng-include>
-  </td>
+    <tr>
+      <td class="sidepanel" style="">
+        <div id="e_search" ng-click="lcurrent_view='views/view_search.php'"             class="panelentry clickme">Search</div>
+        <div id="e_profile" ng-click="lcurrent_view='views/view_profile.php'"           class="panelentry clickme">My Profile</div>
+        <div id="e_patientappts" ng-click="lcurrent_view='views/view_patientappts.php'" class="panelentry clickme">My Appointments (Pt)</div>
+        <div id="e_doctorappts" ng-click="lcurrent_view='views/view_doctorappts.php'"   class="panelentry clickme">My Appointments (Dr)</div>
+      </td>
+    <td id="c_navpages" class="form-style-8" style="overflow:hidden">
+      <ng-include src="lcurrent_view"> </ng-include>
+    </td>
+    </tr>
   </table>
-
+  </div>
 <?php else: ?>
-    <div style="text-align:right">
-      <a href="login.php"> Sign In</a>
-    </div>
-    <div class=boat style="height:600px">
-      <div style="height:20%">
+  <div class='boat'>
+    <h1 class='Neolafia'>
+      <img src='../images/logo.png' style='height:1em;'/>
+      Neolafia
+    </h1>
+    <div>
+      <div class='question-container'>
+        <div class='question'>
+          <h2>Looking for a doctor?</h2>
+          <form method='GET' action='views/doctor_search.php' class="form-style-8">
+            <input name='q' id="ikeyword_search" type="text" ng-model="keyword_search"
+                   autofocus ng-change='update_dropdown()' />
+          </form>
+        </div>
       </div>
-      <div class="banner" style="width:90%">
-        <div style="opacity:2.0">
-
-           <h1>
-            Eku Ojumo!
-          </h1>
-          <h1>
-            Good Morning!
-          </h1>
-
+    </div>  
+  </div>
+  <div class='frontpage-body'>
+    <div class='frontpage-container'>
+      <div class='frontpage-entry'>
+        <h2 class='frontpage-heading'>Are you a Medical Professional?</h2>
+        <div class='frontpage-text'>
+        We could use your help! Make a difference around you by lorem ispum dolor sit amet!
+        </div>
+      </div>
+      <div class='frontpage-entry'>
+        <h2 class='frontpage-heading'>Connect with Medical Specialists near You</h2>
+        <div class='frontpage-text'>
+          Cough that just won't go away? Back pain keeping you up? We make finding
+          a medical specialist easy. Lorem ipsum, sign up to try it out!
         </div>
       </div>
     </div>
-    <div class="form-style-8">
-    <h2>Are you looking for a doctor?</h2>
-    <form method='GET' action='views/doctor_search.php'>
-
-    <input name='q' id="ikeyword_search" type="text" ng-model="keyword_search"
-      autofocus ng-change='update_dropdown()'
-    />
-    <div ng-model="keyword_search">{{keyword_search}}</div>
-    <input type="submit" ng-click="update(user)" value="SEARCH" />
-
-    </form>
-    <h2>Are you a member?</h2>
-    <form>
-      <input type="button" onClick="goto('login.php')" value="Sign In" />
-      <div style="float:left; overflow:hidden; width:20px"></div>
-      <input type="button" onClick="goto('createaccount.php')" value="Create an Account" />
-    </form>
-    </div>
-<?php endif; ?>
   </div>
+<?php endif; ?>
 </body>
 </html>
