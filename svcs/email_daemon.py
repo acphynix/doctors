bb5f45a0140fa7b953b282e2bcc7debf0dabc3c2
Ashwin <ashwinchetty@gmail.com>
2017-08-13 10:59:10 -0500

    daemon

--- svcs/email_daemon.py
+++ svcs/email_daemon.py
@@ -2,37 +2,62 @@
 
 import MySQLdb
 import email
+import boto3
+
+client = boto3.client('ses')
+
+def send_email( send_to, send_bcc, subject, body_html ):
+  response = client.send_email(
+    Source='neolafia@neolafia.com',
+    Destination={
+      'ToAddresses' : send_to,
+      'CcAddresses' : [],
+      'BccAddresses': send_bcc
+    },
+    Message={
+      'Subject': {
+        'Data': subject,
+        'Charset': 'utf-8'
+      },
+      'Body': {
+        'Html': {
+          'Data': body_html,
+          'Charset': 'utf-8'
+        }
+      }
+    },
+    ReplyToAddresses=['neolafia@neolafia.com'],
+    ReturnPath='neolafia@neolafia.com',
+  )
+
+  return response
 
 db = MySQLdb.connect(host="localhost",           # your host, usually localhost
                      user="ec2-user",            # your username
                      passwd="",                  # your password
                      db="HealthTechSchema")      # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor()
 
-def create_email(params):
-  query = """insert into emails
-    (user_id, subject, user_email, content, nature, status, times_sent) values
-    ()
-  """.substitute(**params)
 
 
-# Use all the SQL you like
+# cur.execute("""
+#     insert into emails
+#     (user_id, subject, user_email, content, nature, status, times_sent) values
+#     ('163', 'subject line', 'ashwinchetty@gmail.com', '<html><body>Email <i>contents</i>.</body></html>','update','queued','0')
+#   """)
 
-
-cur.execute("""
-    insert into emails
-    (user_id, subject, user_email, content, nature, status, times_sent) values
-    ('163', 'subject line', 'ashwinchetty@gmail.com', '<html><body>Email <i>contents</i>.</body></html>','update','queued','0')
-  """)
-
+# cur.execute("update emails set subject='el sujeto'")
 cur.execute("SELECT * FROM emails where status='queued' limit 1")
 
 
 # print all the first cell of all the rows
 for row in cur.fetchall():
+  cur.execute("update emails set status='lock' where email_id = "+row[0])
+  db.commit()
+  send_email( [row[3]], [], row[2], row[4])
+  cur.execute("update emails set status='sent',times_sent="+row[7]+" where email_id = "+row[0])
   print row
 
+db.commit()
 db.close()
