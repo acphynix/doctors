06c67e3d8f4ffcd0c4f9ba33de14bd69ada3a925
Ashwin <ashwinchetty@gmail.com>
2017-08-13 09:51:33 -0500

    emails

--- svcs/email_daemon.py
+++ svcs/email_daemon.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-
-print 'hello world!'
-
 import MySQLdb
 import email
 
@@ -15,8 +12,24 @@ db = MySQLdb.connect(host="localhost",           # your host, usually localhost
 #  you execute all the queries you need
 cur = db.cursor()
 
+def create_email(params):
+  query = """insert into emails
+    (user_id, subject, user_email, content, nature, status, times_sent) values
+    ()
+  """.substitute(**params)
+
+
 # Use all the SQL you like
+
+
+cur.execute("""
+    insert into emails
+    (user_id, subject, user_email, content, nature, status, times_sent) values
+    ('163', 'subject line', 'ashwinchetty@gmail.com', '<html><body>Email <i>contents</i>.</body></html>','update','queued','0')
+  """)
+
 cur.execute("SELECT * FROM emails where status='queued' limit 1")
+
 
 # print all the first cell of all the rows
 for row in cur.fetchall():
