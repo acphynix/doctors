312967241e17b9266f770f2a14e0c41a6a5b4a05
Ashwin <ashwinchetty@gmail.com>
2017-08-13 23:05:32 -0500

    email fix

--- html/ajax/post_approve_appointment.php
+++ html/ajax/post_approve_appointment.php
@@ -28,14 +28,15 @@ $params['a'] = sanitize_plaintext($params['a']);
 
 $user_id=$_SESSION['user_id'];
 
-$appt = query_select_from_eq('appointments',array('1'),sprintf("appointment_id=%s and doctor_id=%s and status='paid'", $params['a'], $user_id));
+$appt = query_select_from_eq('appointments',array('doctor_id', 'patient_id'),sprintf("appointment_id=%s and doctor_id=%s and status='paid'", $params['a'], $user_id));
 if(count($appt) == 0){
    header('HTTP/1.1 403 Forbidden');
    return;
 }
 
 query_update( 'appointments', array('status' => 'approved'), sprintf("appointment_id=%s",$params['a']));
-create_email_for_appt( $user_id, 'approved', $params['a'] );
+create_email_for_appt( $user_id, 'doctor_appointment_approved', $params['a'] );
+create_email_for_appt( $appt[0]['patient_id'], 'patient_appointment_approved', $params['a'] );
 
 echo "{'success':'true'}";
 
