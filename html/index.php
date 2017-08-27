4557d70e2ef61738a297441ed1fd55a189dc7faa
Ashwin <ashwinchetty@gmail.com>
2017-08-27 05:58:04 -0500

    analytics

--- html/index.php
+++ html/index.php
@@ -8,6 +8,18 @@
 ?>
 <html ng-app="healthapp">
 <head>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-105319774-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <title>Neolafia</title>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -33,16 +45,6 @@
 <link rel="stylesheet" type="text/css" href="forms.css"> 
 <link rel="stylesheet" type="text/css" href="styles/styles.css"> 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-105319774-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 </head>
 
