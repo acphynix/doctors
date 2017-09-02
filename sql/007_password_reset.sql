11ccb33baa1068e1f04dee58ce2db0aa18678678
Victor <vjovict@gmail.com>
2017-09-02 12:06:59 +0100

    More fixes

---
+++ sql/007_password_reset.sql
@@ -0,0 +1,6 @@
+-- drop table if exists password_reset;
+CREATE TABLE `password_reset` (
+  `user_id` int(10) unsigned NOT NULL,
+  `reset_code` varchar(24) NOT NULL,
+  PRIMARY KEY (`user_id`)
+);
--- svcs/em.py
+++ svcs/em.py
@@ -2,11 +2,7 @@
 
 import boto3
 
-client = boto3.client('ses', 
-          region_name='us-east-1',
-          aws_access_key_id='AKIAI3P25I4PQWG2S7QQ',
-          aws_secret_access_key='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'
-        )
+client = boto3.client('ses')
 
 
 def send_email( send_to, send_bcc, subject, body_html ):
@@ -35,5 +31,5 @@ def send_email( send_to, send_bcc, subject, body_html ):
 
   return response
 
-send_email( ['vjovict@gmail.com'], [], 'test email', '<html><body><p>this is <b>bolded</b> text.</p></body></html>')
+send_email( ['ashwinchetty@gmail.com'], [], 'test email', '<html><body><p>this is <b>bolded</b> text.</p></body></html>')
 
