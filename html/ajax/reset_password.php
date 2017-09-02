db8ef8769f7c6bdaab534457d87864e48d843314
Victor <vjovict@gmail.com>
2017-09-02 16:41:29 +0100

    More fixes

--- html/ajax/reset_password.php
+++ html/ajax/reset_password.php
@@ -16,8 +16,18 @@
     }
     
     $verify_str = bin2hex(random_bytes(10));
-    $sql_addverify = sprintf("insert into password_reset (user_id, reset_code) values (%s,'%s')",$userid,$verify_str);
-    $sres = mysqli_query($database, $sql_addverify);
+    $useridPr = 
+            mysqli_query($database,
+                    sprintf("select user_id from password_reset where user_id='%s' and reset_code=''",$userid))
+            ->fetch_assoc()['user_id'];
+    if($useridPr){
+        $sql = sprintf("update password_reset set reset_code='%s' where user_id='%s'",$verify_str, $userid);
+        $res = mysqli_query($database, $sql);
+    }
+    if(!$useridPr){
+        $sql_addverify = sprintf("insert into password_reset (user_id, reset_code) values (%s,'%s')",$userid,$verify_str);
+        $sres = mysqli_query($database, $sql_addverify);
+    }
     
     create_email($userid, 'user_account_passwordreset');
 
