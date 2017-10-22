ead8c98593b17df724ee44efb7a9efb04a4e8061
Victor <vjovict@gmail.com>
2017-10-22 17:47:23 +0100

    Touch.js plugin for calendar-touch-responsiveness on mobile

--- html/page/home.php
+++ html/page/home.php
@@ -27,8 +27,10 @@ error_reporting(0);
 ?>
 <html ng-app="dashboard">
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
 <script src="/js/touchpunch.js"></script>
 <script>
     $("#widget").draggable();
